0ce509631c851eeabfd3ddee1f0de6e08f779a7e
Algo <algor102@gmail.comm>
2023-02-04 04:37:33 +0500

    1.8Semm-DZ-сравнение цифр в двузначном числе

---
+++ 1.8Semm-DZ-bolshee/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Программа выводит большую цифру у двузначного числа");
+l1:
+Console.WriteLine("Ведите двузначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int posl = number % 10;
+int perv = (number - posl)/10;
+
+if (perv == posl)
+{
+  Console.WriteLine("Первая и вторая цифра одинаковые, введите чило снова");
+  goto l1;
+}
+else
+{
+  if (perv > posl)
+  {
+    Console.WriteLine($"Первая цифра {perv} больше {posl}");
+  }
+  else
+  {
+    Console.WriteLine($"Вторая цифра {posl} больше {perv}");
+  }
+}
