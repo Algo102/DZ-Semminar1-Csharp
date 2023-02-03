336ed042594e5434f582d61651e888c927028bc8
Algo <algor102@gmail.comm>
2023-02-04 03:30:14 +0500

    1.5Semm-DZ-выводит четные

---
+++ 1.5Semm-DZ-chetnDoN/Program.cs
@@ -0,0 +1,26 @@
+﻿// 0 отсекает
+Console.WriteLine("Программа выводит все четные числа до введенного");
+l1:
+Console.WriteLine("Введите цело и положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0)
+  {
+    Console.WriteLine("Вы ввели: " + number + ". Введите число снова");
+    goto l1;
+  }
+
+int count = 1;
+
+while (count < number+1)
+{
+  if (count % 2 == 0)
+  {
+    Console.Write("  " + count);
+    count++;
+  }
+  else
+  {
+    count++;
+  }
+}
