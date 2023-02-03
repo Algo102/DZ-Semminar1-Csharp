56c31895ab2598df846673c9d9f5e904f52753f3
Algo <algor102@gmail.comm>
2023-02-04 03:54:57 +0500

    1.6Semm-DZ-последнее число

---
+++ 1.6Semm-DZ-3chislo/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Программа выводит третью цифру");
+Console.WriteLine("Ведите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int rez = number % 10;
+
+Console.WriteLine("Проверяйте: " + rez);
