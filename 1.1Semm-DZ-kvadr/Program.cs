e0e8111b012d9353ae085e46b128859e3f8904c8
Algo <algor102@gmail.comm>
2023-02-03 08:53:30 +0500

    ДЗ1-Семминар1-Квадрат

---
+++ 1.1Semm-DZ-kvadr/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("По двум заданным числам проверяем является ли первое квадратом второго");
+Console.WriteLine("Ведите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1/number2 == number2)
+{
+  Console.WriteLine("Да, первое число " + (number1) + " является квадратом " + number2);
+}
+else
+{
+  Console.WriteLine(number1 + " НЕ является квадратом " + number2);
+}
