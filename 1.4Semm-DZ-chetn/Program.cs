f63305ec59bea019cc7a3438055528739c49bcb8
Algo <algor102@gmail.comm>
2023-02-04 02:57:17 +0500

    1.4Semm-DZ-проверка на четность

---
+++ 1.4Semm-DZ-chetn/Program.cs
@@ -0,0 +1,23 @@
+﻿// При вводе 0, программа предложит повторить ввод другого числа
+Console.WriteLine("Проверяем является ли число чётным");
+l1:
+
+Console.WriteLine("Ведите свое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int chetn = number % 2;
+if (number == 0)
+{
+  Console.WriteLine("Вы ввели: " + number + ". Введите число снова");
+  goto l1;
+}
+
+if (chetn == 0)
+{
+  Console.WriteLine("Ваше число: " + number + " четное");
+}
+else
+{
+  Console.WriteLine("Ваше число: " + number + " НЕ четное");
+}
+
