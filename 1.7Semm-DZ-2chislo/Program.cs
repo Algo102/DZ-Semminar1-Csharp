07c1c28f3c24ecc66b30048d377e855221dc4b01
Algo <algor102@gmail.comm>
2023-02-04 04:16:18 +0500

    1.6Semm-DZ-предпследнее число

---
+++ 1.7Semm-DZ-2chislo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа выводит вторую цифру");
+Console.WriteLine("Ведите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int posl = number % 10;
+int predposl = (number - posl)/10;
+int rez = predposl % 10;
+
+
+Console.WriteLine("Проверяйте: " + rez);
