3b4ce274ec5748983a30963b4b972537b17b88db
Algo <algor102@gmail.comm>
2023-02-04 02:32:39 +0500

    1.3Semm-DZ-max3

---
+++ 1.3Semm-DZ-max/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ищем максимальное из трех чисел");
+Console.WriteLine("Ведите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 & number1 > number3 )
+{
+  Console.WriteLine("Число " + number1 + " самое большое");
+}
+
+if (number2 > number1 & number2 > number3 )
+{
+  Console.WriteLine("Число " + number2 + " самое большое");
+}
+
+if (number3 > number2 & number3 > number1 )
+{
+  Console.WriteLine("Число " + number3 + " самое большое");
+}
+
+if ((number1 == number2) || (number1 == number3) || (number2 == number3) )
+{
+  Console.WriteLine("Некоторые числа повторяются, пожалуйста повторите ввод");
+}
