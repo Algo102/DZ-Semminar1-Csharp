3c721d270f7131f1ed535588719fff5d3ec6c635
Algo <algor102@gmail.comm>
2023-02-04 06:45:31 +0500

    1.11Semm-DZ-ШЕДЕВР-поиск третьей цифры

---
+++ 1.11Semm-DZ-poisk3chisla/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Программа ещет 3 число");
+l1:
+Console.Write("Вводите цифры числа через пробел ");
+string enterElem = Console.ReadLine();
+string[] word = enterElem.Split(' ');
+
+byte[] arrEnt = new byte[word.Length];
+
+for (int i = 0; i < word.Length; i++)
+{
+  string sum = word[i].Trim();
+  arrEnt[i] = Convert.ToByte(sum);
+}
+
+if (arrEnt.Length < 2)
+{
+  Console.WriteLine ("В веденом числе меньше трех цифр");
+  goto l1;
+}
+else
+{
+  Console.WriteLine("Третья цифра в введеном числе:" + arrEnt[2]);
+}
+
+
+
+
+
