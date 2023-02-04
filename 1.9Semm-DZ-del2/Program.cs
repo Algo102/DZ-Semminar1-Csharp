1f31c6e5ec34cfd300f486b1ae29f4a6b4bfcaa9
Algo <algor102@gmail.comm>
2023-02-04 05:08:39 +0500

    1.9Semm-DZ-удаление 2 цифры у трехзначного числа

---
+++ 1.9Semm-DZ-del2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа удаляет вторую цифру трёхзначного числа");
+Console.WriteLine("Ведите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int three = number % 10;
+int onetwo = (number - three)/10;
+int two = onetwo % 10;
+int twothree = number % 100;
+int one = (number - twothree) / 10;
+int onethree = one + three;
+
+//Console.WriteLine(one + " " + twothree + " " + number + " " + onetwo + " " + two + " " + three + " " + onethree );
+Console.WriteLine("Результат после удаления второго числа " + onethree);
